67e856fe2bd662080f5a0086909f1d9cfd1ab0b5
afancyman1 <1274005171@qq.com>
2023-04-23 22:17:57 +1200

    11

---
+++ start.bat
@@ -0,0 +1,3 @@
+start "" ".\server\server\bin\Debug\net7.0-windows\server.exe"
+start "" ".\WinFormsApp1\WinFormsApp1\bin\Debug\net7.0-windows\WinFormsApp1.exe"
+start "" ".\client\client\bin\Debug\net7.0-windows\client.exe"
